5829ed8b831bd975532bbdcb8b07efd7f041cee1
Michael MacDonald <michael.macdonald@intel.com>
2016-01-28 11:17:33 -0500

    Use svclog instead of applog/liblog

--- cmd/lhsmd/main.go
+++ cmd/lhsmd/main.go
@@ -4,10 +4,9 @@ import (
 	"flag"
 	"log"
 
-	"github.intel.com/hpdd/applog"
-	"github.intel.com/hpdd/liblog"
 	"github.intel.com/hpdd/policy/pdm"
 	"github.intel.com/hpdd/policy/pdm/lhsmd/agent"
+	"github.intel.com/hpdd/svclog"
 
 	_ "github.intel.com/hpdd/policy/pdm/lhsmd/transport/grpc"
 	// _ "github.intel.com/hpdd/policy/pdm/lhsmd/transport/queue"
@@ -23,19 +22,17 @@ func main() {
 	flag.Parse()
 
 	if enableDebug {
-		applog.SetLevel(applog.DEBUG)
-		liblog.Enable()
+		svclog.EnableDebug()
 	}
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	// Setting the prefix helps us to track down deprecated calls to log.*
-	log.SetOutput(applog.Writer().Prefix("DEPRECATED"))
-	liblog.SetWriter(applog.Writer())
+	log.SetOutput(svclog.Writer().Prefix("DEPRECATED"))
 
 	conf := pdm.ConfigInitMust()
 
-	applog.Debug("current configuration:\n%v", conf.String())
+	svclog.Debug("current configuration:\n%v", conf.String())
 
 	if err := agent.Daemon(conf); err != nil {
-		applog.Fail("Error in agent.Daemon(): %s", err)
+		svclog.Fail("Error in agent.Daemon(): %s", err)
 	}
 }
