8fd4a6b3adfe6c78781513c4f5db26728712f3fa
Robert Read <robert.read@intel.com>
2016-03-14 18:49:21 -0700

    Add profiling support
    Build with "go build --tag profile" to enable dynamic profiling.
    See bottom of https://blog.golang.org/profiling-go-programs
    for how to use.

---
+++ cmd/lhsmd/profile.go
@@ -0,0 +1,16 @@
+// +build profile
+
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func init() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+}
