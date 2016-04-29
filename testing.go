4ee6624df8b206abb760d559501991f804f9aded
Robert Read <robert.read@intel.com>
2016-04-29 16:33:19 -0700

    move the client testing.go to client package

---
+++ testing.go
@@ -0,0 +1,28 @@
+package client
+
+import "github.intel.com/hpdd/lustre/fs"
+
+// TestClient implements the client.Client interface
+type testClient struct {
+	root string
+}
+
+func Test(root string) Client {
+	return &testClient{root: root}
+}
+
+// FsName returns a fake filesystem name
+func (c *testClient) FsName() string {
+	return "test"
+}
+
+// Path returns a fake filesystem path
+func (c *testClient) Path() string {
+	return c.root
+}
+
+// Root returns a fake fs.RootDir item
+func (c *testClient) Root() fs.RootDir {
+	// Todo need a TestRootDir
+	return fs.RootDir{path: root}
+}
