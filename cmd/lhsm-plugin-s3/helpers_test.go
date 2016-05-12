492e44b2951de79475b89a7d564bcf0ad4f8c199
Robert Read <robert.read@intel.com>
2016-05-12 12:37:41 -0700

    s3: add unit tests
     - copied from posix. helpers are the same, but tests
       needed some customization.

---
+++ cmd/lhsm-plugin-s3/helpers_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var testPrefix = "ptest"
+
+func testTempDir(t *testing.T) (string, func()) {
+	tdir, err := ioutil.TempDir("", testPrefix)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return tdir, func() {
+		err = os.RemoveAll(tdir)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func testChdirTemp(t *testing.T) func() {
+	tdir, cleanDir := testTempDir(t)
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = os.Chdir(tdir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return func() {
+		err := os.Chdir(cwd)
+		if err != nil {
+			t.Fatal(err)
+		}
+		cleanDir()
+	}
+}
+
+func testFill(t *testing.T, fp *os.File, size uint64) {
+	var bs uint64 = 1024 * 1024
+	buf := make([]byte, bs)
+
+	for i := 0; i < len(buf); i++ {
+		buf[i] = byte(i)
+	}
+
+	for i := uint64(0); i < size; i += bs {
+		if size < bs {
+			bs = size
+		}
+		fp.Write(buf[:bs])
+
+	}
+}
+
+func testCorruptFile(t *testing.T, path string) {
+	fp, err := os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = fp.Write([]byte("Silent data corruption. :)"))
+	if err != nil {
+		t.Fatal(err)
+
+	}
+	err = fp.Close()
+	if err != nil {
+		t.Fatal(err)
+
+	}
+}
+
+func testTempFile(t *testing.T, size uint64) (string, func()) {
+	fp, err := ioutil.TempFile(".", testPrefix)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fp.Close()
+
+	if size > 0 {
+		testFill(t, fp, size)
+	}
+	name := fp.Name()
+	return name, func() {
+		err := os.Remove(name)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func testAction(t *testing.T) string {
+	return "action"
+}
---
+++ cmd/lhsm-plugin-s3/s3_test.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"math"
+	"os"
+	"testing"
+	"time"
+
+	"github.intel.com/hpdd/policy/pdm/dmplugin"
+	//	"github.intel.com/hpdd/policy/pdm/lhsm-plugin-posix/posix"
+)
+
+func testArchive(t *testing.T, mover *Mover, path string, offset uint64, length uint64, fileID []byte, data []byte) *dmplugin.TestAction {
+	action := dmplugin.NewTestAction(t, path, offset, length, fileID, data)
+	if err := mover.Archive(action); err != nil {
+		t.Fatal(err)
+	}
+	return action
+}
+
+func testRemove(t *testing.T, mover *Mover, fileID []byte, data []byte) *dmplugin.TestAction {
+	action := dmplugin.NewTestAction(t, "", 0, 0, fileID, data)
+	if err := mover.Remove(action); err != nil {
+		t.Fatal(err)
+	}
+	return action
+}
+
+func testRestore(t *testing.T, mover *Mover, offset uint64, length uint64, fileID []byte, data []byte) *dmplugin.TestAction {
+	tfile, cleanFile := testTempFile(t, 0)
+	defer cleanFile()
+	action := dmplugin.NewTestAction(t, tfile, offset, length, fileID, data)
+	if err := mover.Restore(action); err != nil {
+		t.Fatal(err)
+	}
+	return action
+}
+
+func testRestoreFail(t *testing.T, mover *Mover, offset uint64, length uint64, fileID []byte, data []byte) *dmplugin.TestAction {
+	tfile, cleanFile := testTempFile(t, 0)
+	defer cleanFile()
+	action := dmplugin.NewTestAction(t, tfile, offset, length, fileID, data)
+	if err := mover.Restore(action); err == nil {
+		t.Fatal("Expected restore to fail")
+	}
+	return action
+}
+
+/*
+func testDestinationFile(t *testing.T, mover *Mover, buf []byte) string {
+	fileID, err := posix.ParseFileID(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return mover.Destination(fileID.UUID)
+}
+*/
+
+func TestArchive(t *testing.T) {
+	WithS3Mover(t, nil, func(t *testing.T, mover *Mover) {
+		// trigger two updates (at current interval of 10MB
+		var length uint64 = 20 * 1024 * 1024
+		tfile, cleanFile := testTempFile(t, length)
+		defer cleanFile()
+
+		start := time.Now()
+		action := testArchive(t, mover, tfile, 0, length, nil, nil)
+
+		// TODO: parameterize the update interval
+		expectedUpdates := int((time.Since(start) / time.Second) / 10)
+
+		if action.Updates != expectedUpdates {
+			t.Errorf("expected %d updates, got %d", expectedUpdates, action.Updates)
+		}
+
+		start = time.Now()
+		restore := testRestore(t, mover, 0, length, action.FileID(), nil)
+		// TODO: parameterize the update interval
+		duration := time.Since(start)
+		expectedUpdates = int((duration / time.Second) / 10)
+		if restore.Updates != expectedUpdates {
+			t.Errorf("expected %d updates, got %d, duration: %v", expectedUpdates, restore.Updates, duration)
+		}
+	})
+}
+
+func TestArchiveMaxSize(t *testing.T) {
+	WithS3Mover(t, nil, func(t *testing.T, mover *Mover) {
+		var length uint64 = 1000000
+		tfile, cleanFile := testTempFile(t, length)
+		defer cleanFile()
+
+		// we received maxuint64 from coordinator, so test this as well
+		action := testArchive(t, mover, tfile, 0, math.MaxUint64, nil, nil)
+		testRestore(t, mover, 0, math.MaxUint64, action.FileID(), nil)
+	})
+}
+
+/*
+func TestArchiveNoChecksum(t *testing.T) {
+	disableChecksum := func(cfg *MoverConfig) *MoverConfig {
+		cfg.Checksums.Disabled = true
+		return cfg
+	}
+
+	WithS3Mover(t, disableChecksum, func(t *testing.T, mover *Mover) {
+		var length uint64 = 1000000
+		tfile, cleanFile := testTempFile(t, length)
+		defer cleanFile()
+
+		action := testArchive(t, mover, tfile, 0, math.MaxUint64, nil, nil)
+		// we received maxuint64 from coordinator, so test this as well
+
+		fileID, err := posix.ParseFileID(action.FileID())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		testCorruptFile(t, mover.Destination(fileID.UUID))
+
+		// Successfully restore corrupt data
+		testRestore(t, mover, 0, math.MaxUint64, action.FileID(), nil)
+	})
+}
+
+func TestArchiveNoChecksumRestore(t *testing.T) {
+	disableChecksum := func(cfg *MoverConfig) *MoverConfig {
+		cfg.Checksums.DisableCompareOnRestore = true
+		return cfg
+	}
+
+	WithS3Mover(t, disableChecksum, func(t *testing.T, mover *Mover) {
+		var length uint64 = 1000000
+		tfile, cleanFile := testTempFile(t, length)
+		defer cleanFile()
+
+		action := testArchive(t, mover, tfile, 0, math.MaxUint64, nil, nil)
+		// we received maxuint64 from coordinator, so test this as well
+
+		fileID, err := posix.ParseFileID(action.FileID())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		testCorruptFile(t, mover.Destination(fileID.UUID))
+		// Successfully restore corrupt data
+		testRestore(t, mover, 0, math.MaxUint64, action.FileID(), nil)
+	})
+}
+
+func TestArchiveChecksumAfter(t *testing.T) {
+	WithS3Mover(t, nil, func(t *testing.T, mover *Mover) {
+		var length uint64 = 1000000
+		tfile, cleanFile := testTempFile(t, length)
+		defer cleanFile()
+
+		// we received maxuint64 from coordinator, so test this as well
+		action := testArchive(t, mover, tfile, 0, math.MaxUint64, nil, nil)
+		// Disable checksum generation but should still check existing checksums
+		mover.ChecksumConfig().Disabled = true
+		testCorruptFile(t, testDestinationFile(t, mover, action.FileID()))
+		// Don't  restore corrupt data
+		testRestoreFail(t, mover, 0, math.MaxUint64, action.FileID(), nil)
+	})
+}
+*/
+
+/*
+func TestCorruptArchive(t *testing.T) {
+	WithS3Mover(t, nil, func(t *testing.T, mover *Mover) {
+		var length uint64 = 1000000
+		tfile, cleanFile := testTempFile(t, length)
+		defer cleanFile()
+
+		action := dmplugin.NewTestAction(t, tfile, 0, length, nil, nil)
+		if err := mover.Archive(action); err != nil {
+			t.Fatal(err)
+		}
+
+		path := testDestinationFile(t, mover, action.FileID())
+
+		testCorruptFile(t, path)
+
+		// TODO check for specific CheckSum error
+		testRestoreFail(t, mover, 0, length, action.FileID(), nil)
+
+	})
+}
+*/
+func TestRemove(t *testing.T) {
+	WithS3Mover(t, nil, func(t *testing.T, mover *Mover) {
+		var length uint64 = 1000000
+		tfile, cleanFile := testTempFile(t, length)
+		defer cleanFile()
+
+		action := testArchive(t, mover, tfile, 0, length, nil, nil)
+
+		testRemove(t, mover, action.FileID(), nil)
+		testRestoreFail(t, mover, 0, length, action.FileID(), nil)
+	})
+}
+
+func WithS3Mover(t *testing.T, updateConfig func(*archiveConfig) *archiveConfig,
+	tester func(t *testing.T, mover *Mover)) {
+	bucketVar := "LHSM_TEST_BUCKET"
+	// Default region to us-east-1
+	region := os.Getenv("AWS_REGION")
+	if region == "" {
+		region = "us-east-1"
+	}
+
+	bucket := os.Getenv(bucketVar)
+	if bucket == "" {
+		t.Skipf("Set %q in environment to test S3 mover.", bucketVar)
+	}
+
+	config := &archiveConfig{
+		Name:   "test-s3",
+		Region: region,
+		Bucket: bucket,
+		Prefix: "ptest",
+	}
+
+	if updateConfig != nil {
+		config = updateConfig(config)
+	}
+
+	defer testChdirTemp(t)()
+	svc := s3Svc(config.Region)
+	mover := S3Mover(svc, 1, config.Bucket, config.Prefix)
+
+	tester(t, mover)
+}
