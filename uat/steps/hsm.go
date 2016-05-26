838c6d69a2bc9a7c85b4f668d29811786dfd8367
Michael MacDonald <michael.macdonald@intel.com>
2016-05-26 12:19:45 -0400

    Break UAT test file creation out into a harness function
    Allow other stuff to leverage this functionality.

--- uat/steps/hsm.go
+++ uat/steps/hsm.go
@@ -1,15 +1,14 @@
 package steps
 
 import (
-	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"regexp"
 
 	"github.com/pkg/errors"
 
 	"github.intel.com/hpdd/logging/debug"
+	"github.intel.com/hpdd/policy/pdm/uat/harness"
 	"github.intel.com/hpdd/policy/pdm/uat/suite"
 )
 
@@ -30,37 +29,7 @@ func getFilePath(cfg *suite.Config, name string) (string, error) {
 		return ctx.GetKey(HSMTestFileKey)
 	}
 
-	// If we're not working with a specific file, then we'll need to
-	// create one. We could use an empty file or fill it with zeros
-	// or something, but that's no fun. Let's try copying the contents
-	// of the test executable out as the HSM test file.
-	out, err := ioutil.TempFile(cfg.LustrePath, HSMTestFileKey)
-	if err != nil {
-		return "", errors.Wrap(err, "Unable to create HSM test file")
-	}
-
-	// Won't work on OS X, but then again none of this will...
-	srcPath, err := os.Readlink("/proc/self/exe")
-	if err != nil {
-		return "", errors.Wrap(err, "Unable to find path to self")
-	}
-
-	in, err := os.Open(srcPath)
-	if err != nil {
-		return "", errors.Wrapf(err, "Failed to open %s for read", srcPath)
-	}
-	defer in.Close()
-
-	if _, err := bufio.NewReader(in).WriteTo(out); err != nil {
-		return "", errors.Wrap(err, "Failed to write data to HSM test file")
-	}
-
-	ctx.AddCleanup(func() error {
-		return os.Remove(out.Name())
-	})
-	ctx.SetKey(HSMTestFileKey, out.Name())
-	debug.Printf("Created HSM test file: %s", out.Name())
-	return out.Name(), out.Close()
+	return harness.CreateTestfile(ctx, cfg.LustrePath, HSMTestFileKey)
 }
 
 func performHSMAction(action, filename string) error {
