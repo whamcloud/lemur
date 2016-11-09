2b18e8086b7cb89ad54b5fdfd4036d6b7068a7af
Michael MacDonald <michael.macdonald@intel.com>
2016-11-09 11:00:32 -0500

    Replace go-ps with homegrown implementation
    We don't need platform independence -- the UAT suite only runs
    on Linux anyhow.

--- uat/steps/util.go
+++ uat/steps/util.go
@@ -6,12 +6,14 @@ package steps
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"path"
+	"strconv"
 	"time"
 
 	"github.com/intel-hpdd/logging/debug"
 
-	"github.com/mjmac/go-ps" // until PR is merged (hopefully)
 	"github.com/pkg/errors"
 )
 
@@ -28,14 +30,82 @@ const (
 	StartupDelay = 500 * time.Millisecond
 )
 
-func findProcess(psName string) (ps.Process, error) {
-	psList, err := ps.Processes()
+type (
+	unixProcess struct {
+		Pid        int
+		Executable string
+	}
+)
+
+func newUnixProcess(pid int) (*unixProcess, error) {
+	p := &unixProcess{Pid: pid}
+	return p, p.GetExe()
+}
+
+func (p *unixProcess) GetExe() error {
+	fullPath, err := os.Readlink(fmt.Sprintf("/proc/%d/exe", p.Pid))
+	p.Executable = fullPath
+
+	return err
+}
+
+func lsProcs() ([]*unixProcess, error) {
+	d, err := os.Open("/proc")
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to open /proc")
+	}
+	defer d.Close()
+
+	// preallocate a bit
+	procs := make([]*unixProcess, 0, 100)
+	for {
+		entries, err := d.Readdir(25)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, errors.Wrap(err, "Error while scanning /proc")
+		}
+
+		for _, entry := range entries {
+			if !entry.IsDir() {
+				continue
+			}
+
+			// Skip non-PID files
+			name := entry.Name()
+			if name[0] < '0' || name[0] > '9' {
+				continue
+			}
+
+			pid, err := strconv.ParseInt(name, 10, 0)
+			if err != nil {
+				return nil, errors.Wrapf(err, "Couldn't parse %s into a pid", name)
+			}
+
+			// At this point, an error is probably due to trying
+			// to look at a process that has already exited by
+			// now, so ignore it.
+			p, err := newUnixProcess(int(pid))
+			if err != nil {
+				continue
+			}
+
+			procs = append(procs, p)
+		}
+	}
+
+	return procs, nil
+}
+
+func findProcess(psName string) (*unixProcess, error) {
+	psList, err := lsProcs()
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to get process list")
 	}
 
 	for _, process := range psList {
-		if path.Base(process.Executable()) == psName {
+		if path.Base(process.Executable) == psName {
 			return process, nil
 		}
 	}
@@ -58,8 +128,8 @@ func checkProcessState(psName, state string, pid int) error {
 		// expected pid.
 		debug.Printf("Checking pid %d of %s", pid, psName)
 		if pid > 0 {
-			if p.Pid() != pid {
-				return errors.Errorf("Found %s, but pid %d does not match expected pid %d!", psName, p.Pid(), pid)
+			if p.Pid != pid {
+				return errors.Errorf("Found %s, but pid %d does not match expected pid %d!", psName, p.Pid, pid)
 			}
 		}
 		return nil
